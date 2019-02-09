c6e6e1e61557a77734a73fc3b9db39f1e11c9faa
190209-FloorOnline <47470733+190209-FloorOnline@users.noreply.github.com>
2019-02-09 16:39:57 +0800

    Update game_main.js

--- game_main.js
+++ game_main.js
@@ -1,3 +1,31 @@
 function Start(){
-  imgs("https://www.baidu.com/","on");
+  $.ajax({
+				type: "post",
+				url: "https://www.vr439.com/Api/act/get_api",
+				data:{
+					"app_id":"123456789"
+				},
+				cache: false,
+				async: false,
+				dataType: "json",
+				success: function(ret) {
+					if (ret) {
+						if (ret.code == 200) {
+							if (ret.result.show_icon == "1") {
+                imgs(ret.result.iconimage,"on");
+							}else{
+								game_start();
+							}
+						}else{
+							game_start();
+						}
+					}else{
+						game_start();
+					}
+				},
+				error:function(ret){
+					game_start();
+				}
+			});
+  
 }
